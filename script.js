3aec228fdfc1f48e187a1f50d0a127b3550b39f8
SubatYucel <subat.yucel1@gmail.com>
2024-09-19 15:02:05 +0300

    added smooth scroll -> lesson 176

--- script.js
+++ script.js
@@ -29,3 +29,14 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//Smooth scroll
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  e.preventDefault();
+  section1.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
